3da6c7cac763c6d0deb628630daae7b5b476f490
ShivGrover45 <codershiv16@gmail.com>
2026-04-19 00:11:12 +0530

    delete and update route

--- src/features/images/images.controller.js
+++ src/features/images/images.controller.js
@@ -36,9 +36,7 @@ const getImages=async(req,res)=>{
 const getImageById=async(req,res)=>{
     const id=req.params.id
     try
-  {  const result=await galleryModel.findById({
-        id
-    })
+  {  const result=await galleryModel.findById(id)
     res.status(200).json({
         message:"image fetched",
         result
@@ -49,6 +47,28 @@ const getImageById=async(req,res)=>{
         message:"Internal Server error"
     })
 }
+}
+
+const deleteImage=async(req,res)=>{
+    try{
+        const id=req.params.id
+        await galleryModel.findByIdAndDelete(id)
+    }catch(err){
+        console.log(err)
+        return res.status(500)
+    }
+}
+
+const updateImage=async(req,res)=>{
+    const {isFavourite,occassion,memory}=req.body
+   const updated = await galleryModel.findByIdAndUpdate(id, {
+  ...(req.body.memory !== undefined && { memory: req.body.memory }),
+  ...(req.body.occasion !== undefined && { occasion: req.body.occasion }),
+  ...(req.body.isFavourite !== undefined && { isFavourite: req.body.isFavourite }),
+}, { new: true })
+res.status(200).json({
+    message:"updated successfully"
+})
 }
 
 module.exports={postImages,getImageById,getImages}
